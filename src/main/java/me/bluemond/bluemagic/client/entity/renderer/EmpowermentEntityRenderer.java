56a5a08f1cf7acd761c240ae1ea18cca93f6a828
bernie-g <berniegandin@gmail.com>
2020-09-30 11:53:43 -0400

    Fixed renderer

--- src/main/java/me/bluemond/bluemagic/client/entity/renderer/EmpowermentEntityRenderer.java
+++ src/main/java/me/bluemond/bluemagic/client/entity/renderer/EmpowermentEntityRenderer.java
@@ -9,10 +9,13 @@ import me.bluemond.bluemagic.entities.EmpowermentEntity;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.client.renderer.entity.model.EntityModel;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.util.ResourceLocation;
+import software.bernie.geckolib.render.IModelRenderer;
 
-public class EmpowermentEntityRenderer extends EntityRenderer<EmpowermentEntity> {
+public class EmpowermentEntityRenderer extends EntityRenderer<EmpowermentEntity> implements IModelRenderer
+{
 
     protected static final ResourceLocation TEXTURE = new ResourceLocation(BlueMagic.MOD_ID, "textures/entity/empowerment_entity.png");
     protected final EmpowermentEntityModel empowermentModel = new EmpowermentEntityModel();
@@ -29,6 +32,7 @@ public class EmpowermentEntityRenderer extends EntityRenderer<EmpowermentEntity>
     @Override
     public void render(EmpowermentEntity entityIn, float entityYaw, float partialTicks, MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int packedLightIn) {
         this.empowermentModel.setRotationAngles(entityIn, partialTicks, 0.0F, -0.1F, 0.0F, 0.0F);
+        this.empowermentModel.setLivingAnimations(entityIn, 0, 0, partialTicks);
         IVertexBuilder ivertexbuilder = bufferIn.getBuffer(this.empowermentModel.getRenderType(this.getEntityTexture(entityIn)));
         this.empowermentModel.render(matrixStackIn, ivertexbuilder, packedLightIn, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
 
@@ -38,5 +42,11 @@ public class EmpowermentEntityRenderer extends EntityRenderer<EmpowermentEntity>
     @Override
     public ResourceLocation getEntityTexture(EmpowermentEntity entity){
         return TEXTURE;
+    }
+
+    @Override
+    public EntityModel getEntityModel()
+    {
+        return this.empowermentModel;
     }
 }
